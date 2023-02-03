80fcd85ba3de9a2e7fff6dcc917dfd2e4ea9bdda
Vikk_Moor <victimofuroboros@gmail.com>
2023-02-04 01:13:52 +0500

    task4_ready

---
+++ sem_one/task4.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the company's revenue: "))
+b = int(input("Enter the company's costs: "))
+if b > a:
+    print(f'The financial result - the loss. Its value is: {b - a}.')
+elif a > b:
+    print(f'The financial result - the profit. Its value is: {a - b}.')
+    print("So, let's calculate the rentability (profit/revenue)")
+    print(f'The rentability value = {round(((a - b) / a), 1)}')
+    c = int(input("Indicate the number of employees: "))
+    print(f'Company profit per employee: {round((a-b)/c, 1)}')
+else:
+    print("Company at breakeven point.")
