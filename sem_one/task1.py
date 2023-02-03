79f24d9e3c196a0f06e452eb86dfec400e358d58
Vikk_Moor <victimofuroboros@gmail.com>
2023-02-03 21:05:12 +0500

    first_ready

---
+++ sem_one/task1.py
@@ -0,0 +1,6 @@
+a = input('Please, enter your name: ')
+b = input("Please, enter your pet's name: ")
+c = int(input('Enter your age: '))
+d = int(input("Enter your pet's age: "))
+
+print(f'{a}, {b}, {c+d}')
