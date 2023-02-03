7017117a60c7cb184e69d047c1e4d39318a1de73
Vikk_Moor <victimofuroboros@gmail.com>
2023-02-03 21:15:06 +0500

    task2_ready

--- sem_one/task1.py
+++ sem_one/task1.py
@@ -3,4 +3,4 @@ b = input("Please, enter your pet's name: ")
 c = int(input('Enter your age: '))
 d = int(input("Enter your pet's age: "))
 
-print(f'{a}, {b}, {c+d}')
+print(f'{a}, {b}, {c + d}')
---
+++ sem_one/task2.py
@@ -0,0 +1,4 @@
+a = int(input('Enter number of seconds: '))
+b = round(a / 60, 1)
+c = round(a / 3600, 1)
+print(f'{c} : {b} : {a}')
