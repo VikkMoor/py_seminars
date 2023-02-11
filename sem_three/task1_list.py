1068229c1d1c8d1608b0e7cf44eb0b0dd7b30791
Vikk_Moor <victimofuroboros@gmail.com>
2023-02-11 17:02:56 +0500

    task1_list_changed

--- sem_three/task1_list.py
+++ sem_three/task1_list.py
@@ -1,10 +1,16 @@
 """Task bout month (list)"""
 mon = int(input('Enter number of month, please: '))
-if 8 < mon < 11:
-    print('autumn')
-elif 2 < mon < 6:
-    print('spring')
-elif 5 < mon < 9:
-    print('summer')
-else:
-    print('winter')
+mon_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+mon_list = ''
+for i in range(len(mon_num)):
+    if 2 < mon_num[i] < 6:
+        mon_list = 'spring'
+    elif 5 < mon_num[i] < 9:
+        mon_list = 'summer'
+    elif 8 < mon_num[i] < 11:
+        mon_list = 'autumn'
+    else:
+        mon_list = 'winter'
+    if mon == mon_num[i]:
+        print(*{mon_list})
+
